eda6a0a1b86dad85f8d684b15379a169ee197cdc
Jonathan Dowland <jon@dow.land>
2018-01-05 16:17:48 +0000

    add partToDot convenience function
    Useful for examining the output of createPartitions
    
    required some small toDot refactoring

--- src/Striot/Util.hs
+++ src/Striot/Util.hs
@@ -1,9 +1,12 @@
-module Striot.Util ( toDot ) where
+module Striot.Util ( toDot, partToDot ) where
 
 import Algebra.Graph
 
 toDot :: Ord a => Show a => Graph a -> String
-toDot g = "digraph {\n" ++ vertexDefs ++ (toDot' (edgeList g)) ++ "}\n" where
+toDot g = "digraph {\n" ++ (toDotMiddle g) ++ "}\n"
+
+toDotMiddle :: Ord a => Show a => Graph a -> String
+toDotMiddle g = vertexDefs ++ (toDot' (edgeList g)) where
     toDot' [] = ""
     toDot' (e:es) = (blah e) ++ (toDot' es)
     blah (v1,v2) = "\t\"" ++ (show' v1) ++ "\" -> \"" ++ (show' v2) ++ "\";\n"
@@ -18,3 +21,9 @@ escape (s:ss) = if   s `elem` escapeme
     where
         escapeme = "\\\":"
     --safechars = concat [['a'..'z'],['A'..'Z'],['\200'..'\377'],"_"]
+
+-- convenience for the output of createPartitions
+--([Graph (StreamVertex a)], [Graph (StreamVertex a)])
+partToDot :: Ord a => Show a => ([Graph a], [Graph a]) -> String
+partToDot ([],_) = ""
+partToDot (parts,_) = "digraph {\n" ++ (concatMap toDotMiddle parts) ++ "}\n"
