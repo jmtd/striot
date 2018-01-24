b5e86140caaee76a3114a9d2b7914716616daee9
Jonathan Dowland <jon@dow.land>
2018-01-24 17:09:41 +0000

    CompileIoTAlga: expansions for code generation
    changes to support porting VizGraph over to CompileIoTAlga, also
    expanding data structures to have the same richness as those in
    CompileIoT so eventual code generation will work.

--- src/Striot/CompileIoTAlga.hs
+++ src/Striot/CompileIoTAlga.hs
@@ -10,14 +10,17 @@ module Striot.CompileIoTAlga ( createPartitions    -- used by Optimizer
                           -- , StreamGraph(..)     -- used by Optimizer
                           -- , StreamOperation(..) -- used by Optimizer
                              , StreamVertex
+                             , vertexId, operator
                              , StreamOperator      -- used by Optimizer
                           -- , Partition           -- used by Optimizer
                           -- , Id
                           -- , createPartitionsAndEdges
                           -- , graphEdgesWithTypes -- used by VizGraph
                           -- , printParams         -- used by VizGraph
-                          -- , generateCode
+                             , generateCode
+                             , PartitionMap -- implementation feature of this module
                              , htf_thisModulesTests
+                             , s0, s1, s2
                              ) where
 
 import Algebra.Graph
@@ -38,15 +41,22 @@ data StreamOperator a = Map       -- (a -> b)      -> Stream a           -> Stre
 
 -- Id needed for uniquely identifying a vertex. (Is there a nicer way?)
 data StreamVertex a = StreamVertex
-    { vertexId :: Int
-    , operator :: StreamOperator a
+    { vertexId   :: Int
+    , operator   :: StreamOperator a
+    , parameters :: [String] -- XXX strings of code. From CompileIoT. Variable length e.g.FilterAcc takes 3 (?)
+    -- this is conflated with StreamOperator's parameter above; I'd prefer to add more params there.
     } deriving (Eq)
 
 instance Ord a => Ord (StreamVertex a) where
-    compare (StreamVertex x _) (StreamVertex y _) = compare x y
+    compare (StreamVertex x _ _) (StreamVertex y _ _) = compare x y
 
 instance Show a => Show (StreamVertex a) where
-    show v = (show (vertexId v))++":"++(show (operator v))
+    show v = (map (\x->if x=='"' then '\'' else x) (show (operator v))) ++
+        (concatMap (\s->"("++s++") ") (parameters v))
+    -- XXX: temporary translate " to ' so output is "dot syntax clean"
+
+-- XXX: add a type definition for Graph (StreamVertex a)... and/or concrete instead of a?
+-- …for exporting
 
 ------------------------------------------------------------------------------
 -- StreamGraph Partitioning
@@ -74,21 +84,25 @@ unPartition (a,b) = foldl overlay Empty (a ++ b)
 -- tests / test data
 
 -- Source -> Sink
-s0 = connect (Vertex (StreamVertex 0 (Source "?"))) (Vertex (StreamVertex 1 (Sink "!")))
+s0 = connect (Vertex (StreamVertex 0 (Source "?") [])) (Vertex (StreamVertex 1 (Sink "!") []))
 
 -- Source -> Filter -> Sink
-s1 = path [StreamVertex 0 (Source "?"), StreamVertex 1 Filter, StreamVertex 2 (Sink "!")]
+s1 = path [StreamVertex 0 (Source "?") [], StreamVertex 1 Filter [], StreamVertex 2 (Sink "!") []]
 
 -- port of s1 from CompileIoT
-s2 = path [ StreamVertex 0 (Source "sourceGen") -- ["sourceGen"]                                                                           "Stream Trip"            ["Taxi.hs","SourceGenerator.hs"],
-          , StreamVertex 1 Map                  -- ["\\t-> Journey{start=toCellQ1 (pickup t), end=toCellQ1 (dropoff t)}"]                  "Stream Journey"         ["Taxi.hs"],
-          , StreamVertex 2 Filter               -- ["\\j-> inRangeQ1 (start j) && inRangeQ1 (end j)"]                                      "Stream Journey"         ["Taxi.hs"],
-          , StreamVertex 3 Window               -- ["slidingTime 1800"]                                                                    "Stream [Journey]"       ["Taxi.hs"],
-          , StreamVertex 4 Map                  -- ["mostFrequent 10"]                                                                     "Stream [(Journey,Int)]" ["Taxi.hs"],
-          , StreamVertex 5 FilterAcc            -- ["value $ head s","\\h acc-> if (h==acc) then acc else h","\\h acc->(h/=acc)","tail s"] "Stream [(Journey,Int)]" ["Taxi.hs"],
-          , StreamVertex 6 (Sink "print")       -- ["print"]                                                                               ""                                []]
+s2 = path [ StreamVertex 0 (Source "sourceGen") ["sourceGen"]--                                                                           "Stream Trip"            ["Taxi.hs","SourceGenerator.hs"],
+          , StreamVertex 1 Map                  ["\\t-> Journey{start=toCellQ1 (pickup t), end=toCellQ1 (dropoff t)}"]--                  "Stream Journey"         ["Taxi.hs"],
+          , StreamVertex 2 Filter               ["\\j-> inRangeQ1 (start j) && inRangeQ1 (end j)"]--                                      "Stream Journey"         ["Taxi.hs"],
+          , StreamVertex 3 Window               ["slidingTime 1800"] --                                                                   "Stream [Journey]"       ["Taxi.hs"],
+          -- StreamOperation opid [opinputs] operator [parameters: "mostFrequent 10"] outputtype:"Stream [(Journey,Int)]" imports:["Taxi.hs"],
+          , StreamVertex 4 Map                  ["mostFrequent 10"] --                                                                    "Stream [(Journey,Int)]" ["Taxi.hs"],
+          , StreamVertex 5 FilterAcc            ["value $ head s","\\h acc-> if (h==acc) then acc else h","\\h acc->(h/=acc)","tail s"]-- "Stream [(Journey,Int)]" ["Taxi.hs"],
+          , StreamVertex 6 (Sink "print")       ["print"] --                                                                              ""                                []]
           ]
 
 test_reform_s0 = assertEqual s0 (unPartition $ createPartitions s0 [[0],[1]])
 test_reform_s1 = assertEqual s1 (unPartition $ createPartitions s1 [[0,1],[2]])
 test_reform_s1_2 = assertEqual s1 (unPartition $ createPartitions s1 [[0],[1,2]])
+
+type StreamGraph = Graph (StreamGraph String)
+generateCode:: StreamGraph -> PartitionMap -> [String] {-stdImports-} -> [String]
