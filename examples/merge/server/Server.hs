ee9f88cf30e9d9e9c8ca86079df4ade4b8f35573
Jonathan Dowland <jon@dow.land>
2018-03-23 11:39:09 +0000

    simplify merge example
    don't use streamWindow in the merge example

--- examples/merge/server/Server.hs
+++ examples/merge/server/Server.hs
@@ -9,11 +9,8 @@ import Striot.Nodes
 sink1 :: Show a => [a] -> IO ()
 sink1 = mapM_ (putStrLn . show)
 
-streamGraphFn :: Stream String -> Stream String -> Stream [String]
-streamGraphFn s1 s2 = let
-       n1 = streamMerge [s1,s2]
-       n2 = streamWindow ((chop 2)) n1
-       in n2
+streamGraphFn :: Stream String -> Stream String -> Stream String
+streamGraphFn s1 s2 = streamMerge [s1,s2]
 
 main :: IO ()
 main = nodeSink2 streamGraphFn sink1 9001 9002
