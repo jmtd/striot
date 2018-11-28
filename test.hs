c1e511a37da7dad3ab6a03e2d93ee2a9601e446a
Jonathan Dowland <jon@dow.land>
2018-11-28 18:42:49 +0000

    remove old comment, go down the rhs-as-function route

--- test.hs
+++ test.hs
@@ -12,32 +12,16 @@ lhs = path
     [ StreamVertex 0 Filter ["f","s"] "a"
     , StreamVertex 1 Filter ["g","s"] "a"
     ]
--- XXX type different to lhs
-rhs = StreamVertex 0 Filter ["\\x -> f x && g x", "s"] "a"
--- Or, RHS as a function?
 
-rhs2 :: StreamGraph -> StreamGraph
-rhs2 (Connect (Vertex (StreamVertex i Filter (f1:_) intype))
-              (Vertex (StreamVertex _ Filter (f2:_) _))) =
+-- RHS as a function
+-- in fact this encodes the whole thing: how to match and what rewrite to apply
+rhs :: StreamGraph -> StreamGraph
+rhs (Connect (Vertex (StreamVertex i Filter (f1:_) intype))
+             (Vertex (StreamVertex _ Filter (f2:_) _))) =
     Vertex $ StreamVertex i Filter ["\\f g x -> f x && g x", f1, f2, "s"] intype
-    -- XXX perhaps, we could use pattern matching to both chekc *and* apply rewrites,
-    -- with a catch-all pattern for "no match, no op"
+-- perhaps, we could use pattern matching to both check *and* apply rewrites,
+-- with a catch-all pattern for "no match, no op"
 rhs2 g = g
-
-{- 
-
-    the vertex id is superfluous.
-    the stream type is probably superfluous (could a rewrite rule
-    change the type of the stream? most likely not.)
-    the variables embedded in the LHS are buried in a string in the
-    RHS
-
-    next step: how would we go about matching the lhs against an
-    instance of streamgraph and seeing if it applies?
-
-    ...the above doesn't even need StreamVertex to be based in terms
-    of an abstract type
--}
 
 exampleStream = path
     [ StreamVertex 1 Source ["return 6"]                         "String"
