21efab0d6d189eed6bb7a0b1149273ce923299cd
Jonathan Dowland <jon@dow.land>
2018-11-28 19:30:12 +0000

    encode another rule, map into filter
    Using pattern matching gets ugly quite quickly and we can't use tricks
    like the Num instance for Graph for clarity since we are restricted to
    constructors

--- test.hs
+++ test.hs
@@ -42,3 +42,26 @@ entryNodes g = let
 
 pp = foldg "()" (show.operator) (wrap " + ") (wrap " * ")
     where wrap x y z = y ++ x ++ z
+
+ppp :: StreamGraph -> String
+ppp = foldg "()" (show) (wrap " + ") (wrap " * ")
+    where wrap x y z = y ++ x ++ z
+          bracket x = "("++x++")"
+
+-- perhaps we should just try to exhaustively apply the function to every subgraph
+-- in the graph? subgraph of size 2 that is
+
+-- some more encoded rules
+-- streamFilter p . streamMap f = streamMap f . streamFilter (p . f)
+mapFilter :: StreamGraph -> StreamGraph
+mapFilter (Connect mapv@(Vertex (StreamVertex i Map (f:fs) intype))
+                   (Vertex (StreamVertex j Filter (p:ps) _)))
+    = sv i Filter (("("++p++").("++f++")"):ps) intype `Connect` mapv
+
+mapFilterEx = (Connect (Vertex (StreamVertex 0 Map ["show"] "Int"))
+                       (Vertex (StreamVertex 1 Filter ["\\x -> length x <3"] "String")))
+
+
+-- attempt to make defining these more compact
+sv a b c d = Vertex (StreamVertex a b c d)
+mapFilterEx3 = (sv 0 Map ["f"] "Int") `Connect` (sv 1 Filter ["p"] "String")
