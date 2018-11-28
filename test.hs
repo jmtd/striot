fd5488d16054519b91d15d7738c5f0204c3b2347
Jonathan Dowland <jon@dow.land>
2018-11-28 19:32:49 +0000

    fix subtle bug about vertexId
    need to be very careful writing these rules! the output of mapFilter
    had two vertexes with the same id, which meant they are collapsed in
    the graph representation (e.g. vertexList $ mapFilter (...) is a one
    element list)

--- test.hs
+++ test.hs
@@ -51,17 +51,18 @@ ppp = foldg "()" (show) (wrap " + ") (wrap " * ")
 -- perhaps we should just try to exhaustively apply the function to every subgraph
 -- in the graph? subgraph of size 2 that is
 
+-- attempt to make defining these more compact
+sv a b c d = Vertex (StreamVertex a b c d)
+
 -- some more encoded rules
 -- streamFilter p . streamMap f = streamMap f . streamFilter (p . f)
 mapFilter :: StreamGraph -> StreamGraph
 mapFilter (Connect mapv@(Vertex (StreamVertex i Map (f:fs) intype))
                    (Vertex (StreamVertex j Filter (p:ps) _)))
-    = sv i Filter (("("++p++").("++f++")"):ps) intype `Connect` mapv
+    = sv j Filter (("("++p++").("++f++")"):ps) intype `Connect` mapv
 
 mapFilterEx = (Connect (Vertex (StreamVertex 0 Map ["show"] "Int"))
                        (Vertex (StreamVertex 1 Filter ["\\x -> length x <3"] "String")))
 
 
--- attempt to make defining these more compact
-sv a b c d = Vertex (StreamVertex a b c d)
 mapFilterEx3 = (sv 0 Map ["f"] "Int") `Connect` (sv 1 Filter ["p"] "String")
