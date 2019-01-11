7c915e0da85b06bfaf485b61806fd2b433efab24
Jonathan Dowland <jon@dow.land>
2019-01-11 14:49:20 +0000

    encode streamFilterAcc rule etc

--- src/TaxiRewrite.hs
+++ src/TaxiRewrite.hs
@@ -15,11 +15,38 @@ import Control.Monad (liftM2)
 -- XXX more descriptive name
 zomg f g x = f x && g x
 
+-- hacks to encapsulate filterAcc fusion components
+fusedAcc  accfn1 accfn2 pred1         (x,y) v = (accfn1 x v, if pred1 v x then accfn2 y v else y)
+fusedPred               pred1 pred2   x (y,z) = pred1 x y && pred2 x z
+comma = (,)
+
 -- XXX: encode the remaining transformations in particular streamJoin and streamFilterAcc transformations
-mylaws = map (parse law) [ "defn filterfuse: streamFilter f . streamFilter g = streamFilter (zomg f g)"
-                         , "defn mapfuse:    streamMap f . streamMap g = streamMap (f . g)"
-                         , "defn mapFilter:  streamFilter p . streamMap f = streamMap f . streamFilter (p . f)"
-                         , "defn mapWindow:  streamWindow w . streamMap f = streamMap (map f) . streamWindow w"
+mylaws = map (parse law) [ {-1-} "defn filterfuse: streamFilter f . streamFilter g = streamFilter (zomg f g)"
+                           {-2,3,4  versions of filterAcc/filter fusion (XXX explicitly number each instance)-}
+                         , {-5-} "defn mapFilter:  streamFilter p . streamMap f = streamMap f . streamFilter (p . f)"
+                         , {-6-} "defn mapfuse:    streamMap f . streamMap g = streamMap (f . g)"
+                         , {-7-} "defn mapWindow:  streamWindow w . streamMap f = streamMap (map f) . streamWindow w"
+
+                         {-  8 join, skipping -}
+                         {-  9-12 expand, skipping -}
+                         {-  13,14 merge, skipping -}
+
+                         {- 15-18 inversion caveat, skipping -}
+                         {- 19,20, expand, skipping -}
+                         {- 21,22 merge, skipping --}
+                         {- 23-26 merge/expand and inverses, skipping -}
+
+
+                         {- streamFilterAcc . streamFilter is built up in two phases
+                                    streamFilter promoted to streamFilterAcc (see promote, below)
+                                    application of streamFilterAcc . streamFilterAcc
+                            thus we don't need a separate rule for streamFilterAcc . streamFilter
+                         -}
+
+                         , "defn filterAccfilterAcc: \
+\                          streamFilterAcc f2 a2 p2 . streamFilterAcc f1 a1 p1\
+\                        = streamFilterAcc (fusedAcc f1 f2 p1) (comma a1 a2) (fusedPred p1 p2)"
+                         -- XXX do we also need to remove the tuples? yes, the parser is fucked
 
                          -- XXX this rule is only useful if it opens up other possibilies, i.e., we can fuse
                          -- two adjacent streamFilterAcc afterwards. I'd also like more assurance that it is
@@ -27,7 +54,13 @@ mylaws = map (parse law) [ "defn filterfuse: streamFilter f . streamFilter g = s
                          , "defn promote:    streamFilter p = streamFilterAcc (flip const) p (p . const)"
                          ]
 
-taxiQ1 = parse expr "streamFilterAcc accfn acc p2 . streamMap f . streamWindow w . streamFilter p . streamMap f2"
+taxiQ1 = parse expr $ concat [ "streamFilterAcc accfn acc p2" -- journeyChanges
+                             , " . streamMap f"               -- topk
+                             , " . streamWindow w"            -- slidingTime 1800 (part of: streamWindowAggregate)
+                             , " . streamFilter p"            -- inRange && inRange
+                             , " . streamMap f2"              -- tripToJourney
+                             ]
+
 foo7 = calculate mylaws taxiQ1
 
 -- decomposed representation of the streamgraph for taxiQ2. The representation of the duplicate streams into
@@ -38,3 +71,7 @@ taxiQ2 = parse expr "streamFilterAcc a b p1 . streamMap f1 . streamWindow w1 \
                                             \ (streamFilter p2 . streamMap f3)"
 
 foo8 = calculate mylaws taxiQ2
+
+-- test filterAcc fusion
+foo9 = calculate mylaws (parse expr "streamFilterAcc a b p1 . streamFilterAcc c d p2")
+fooA = calculate mylaws (parse expr "streamFilterAcc a b p1 . streamFilter p2")
