8aa8980eaec11e506e57e767b36bea864b97ff75
Jonathan Dowland <jon@dow.land>
2019-10-02 15:11:45 +0100

    tidy up; attempt tests; accepting quasi-quotes…

--- TH.hs
+++ TH.hs
@@ -1,39 +1,25 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE DeriveLift #-}
-{-# LANGUAGE OverloadedStrings #-}
 
+-- not sure necessary
+{- LANGUAGE DeriveLift -}
+{- LANGUAGE OverloadedStrings -}
 
 import Striot.FunctionalProcessing
 import Striot.FunctionalIoTtypes
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax -- (Quasi, lift…)
-
-foo = streamMap (+1)
-bar = streamMap (+2)
-baz = foo . bar
-
-huh = [| baz |] -- : Language.Haskell.TH.Lib.Internal.ExpQ
--- lovely.
--- needs passing through runQ get a bare exp "VarE Main.baz"
-
-
--- we want to write a function that takes an argument that is
--- a stream processing thing (type Stream a, perhaps) and looks
--- at it to determine if it's two maps, say, that we can fuse
-
--- how to "visualise" ExpQs?
+import Test.Framework
 
 ------------------------------------------------------------------------------
 -- a function that can identify whether there's a "streamMap" embedded
 -- in the supplied expression
 -- 
 
--- unused
-findMap :: Lift a => Stream a -> IO Bool
-findMap str = do
-    runQ [| str |] >>= print
-    exp <- runQ [| str |]
-    return $ findMap' exp
+-- e.g. findMap [| streamMap (+3) |]
+-- substituting IO for Quasi works
+--findMap :: Quasi m => Q Exp -> m Bool
+findMap expq = runQ expq >>= return . findMap'
 
 findMap' exp = case exp of
         ListE [] -> False
@@ -45,10 +31,25 @@ findMap' exp = case exp of
 -- permits using in the style of blah, below
 findMap'' exp = return $ findMap' exp
 
+-- an example of returning the actual input code
+-- this doesn't work because: • GHC stage restriction:
+--   ‘exp’ is used in a top-level splice, quasi-quote, or annotation,
+--   and must be imported, not defined locally
+{-
+findMap''' expq = do
+    exp <- runQ expq
+    return $ if   findMap' exp
+             then $( exp )
+             else streamSrc
+-}
+
 streamSrc = map (\n -> (Event 0 Nothing (Just n))) [1,2,4,5]
 
 blah :: Quasi m => m Bool
 blah = runQ [| streamMap (+1) streamSrc |] >>= findMap'' -- True
+
+-- can't embed, we need someting that runs Quasi m
+--test_blah = assertBool blah
 
 -- temporarily ignore the time argument to avoid having to implement Lift for
 -- UTCTime and all constituent types
