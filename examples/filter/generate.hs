abc065b8afdeaec29e142533bb03cff1569007ac
Jonathan Dowland <jon@dow.land>
2020-05-10 11:07:17 +0100

    WIP: initial hacking of filter example for TH

--- examples/filter/generate.hs
+++ examples/filter/generate.hs
@@ -5,6 +5,7 @@
 import Striot.CompileIoT
 import Striot.StreamGraph
 import Algebra.Graph
+import Language.Haskell.TH
 
 opts = GenerateOpts { imports   = ["Striot.FunctionalIoTtypes", "Striot.FunctionalProcessing", "Striot.Nodes", "Control.Concurrent", "System.Random"]
                     , packages  = ["random"]
@@ -19,13 +20,15 @@ source = "do\n\
 \        putStrLn $ \"client sending \" ++ s\n\
 \        return s"
 
-v1 = StreamVertex 1 Source [source]                         "String" "String"
-v2 = StreamVertex 2 Map    ["id", "s"]                   "String" "String"
-v3 = StreamVertex 3 Filter ["(\\i -> (read i :: Int) > 5)", "s"] "String" "String"
-v4 = StreamVertex 4 Window ["(chop 1)", "s"] "String" "[String]"
-v5 = StreamVertex 5 Sink   ["mapM_ $ putStrLn . (\"receiving \"++) . show . value"] "[String]" "IO ()"
+exp' = LitE (IntegerL 5)
 
-graph = path [v1, v2, v3, v4, v5]
+graph = simpleStream
+ [ (Source , [exp'], "String") -- source
+ , (Map    , [exp'], "String") --["id", "s"]
+ , (Filter , [exp'], "String") --["(\\i -> (read i :: Int) > 5)", "s"]
+ , (Window , [exp'], "String") --["(chop 1)", "s"]
+ , (Sink   , [exp'], "[String]")  --["mapM_ $ putStrLn . (\"receiving \"++) . show . value"]
+ ]
 
 parts = [[1,2],[3,4,5]]
 
