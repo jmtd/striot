3cc49c03b2e395be6fb47ead20af1a750b566810
Jonathan Dowland <jon@dow.land>
2020-06-05 10:52:22 +0100

    Update remaining examples for StreamVertex change
    All work except for "expand", which is exposing a problem with
    hidden GHC packages/modules: "words" and "unwords" are canonicalized
    to "Data.OldList.(un)words" which cannot be imported. I'm working on
    a solution to this issue (which is not expand specific and could
    crop up anywhere)

--- examples/expand/generate.hs
+++ examples/expand/generate.hs
@@ -2,45 +2,36 @@ import Striot.CompileIoT
 import Striot.StreamGraph
 
 import Algebra.Graph
+import Control.Monad (replicateM)
+import System.Random
 
-opts = GenerateOpts { imports = [ "Striot.FunctionalIoTtypes"
-                                , "Striot.FunctionalProcessing"
-                                , "Striot.Nodes"
-                                , "Control.Concurrent"
-                                , "System.Random (getStdRandom, randomR)"
-                                , "Control.Monad (replicateM)"
-                                ]
-                    , packages  = ["random"]
-                    , preSource = Nothing
-                    , rewrite = False
-                    }
+source = [| do
+    indices <- replicateM 10 (getStdRandom (randomR (0,39)) :: IO Int)
+    let
+        -- 40 words picked randomly from the dictionary, 20 of which are prefixed
+        -- with # to simulate hashtags
+        randomWords = words
+            "Angelica #Seine #sharpened sleeve consonance diabolically\
+            \ #bedlam #sharpener sentimentalizing amperage #quilt Ahmed #quadriceps Mia\
+            \ #burglaries constricted julienne #wavier #gnash #blowguns wiping somebodies\
+            \ nematode metaphorical Chablis #taproom disrespects #oddly ideograph rotunda\
+            \ #verdigrised #blazoned #murmuring #clover #saguaro #sideswipe faulted brought\
+            \ #Selkirk #Kshatriya"
 
--- 40 words picked randomly from the dictionary, 20 of which are prefixed
--- with # to simulate hashtags
-randomWords = words "Angelica #Seine #sharpened sleeve consonance diabolically\
-\ #bedlam #sharpener sentimentalizing amperage #quilt Ahmed #quadriceps Mia\
-\ #burglaries constricted julienne #wavier #gnash #blowguns wiping somebodies\
-\ nematode metaphorical Chablis #taproom disrespects #oddly ideograph rotunda\
-\ #verdigrised #blazoned #murmuring #clover #saguaro #sideswipe faulted brought\
-\ #Selkirk #Kshatriya"
+        s = unwords $ map (randomWords !!) indices in do
+        threadDelay 1000000
+        putStrLn $ "sending " ++ (show s)
+        return s
+    |]
 
-source = "do\n\
-\    indices <- replicateM 10 (getStdRandom (randomR (0,39)) :: IO Int)\n\
-\    let s = unwords $ map (randomWords !!) indices in do\n\
-\        threadDelay 1000000\n\
-\        putStrLn $ \"sending \" ++ (show s)\n\
-\        return s where\n\
-\            randomWords = " ++ (show randomWords)
+v1 = StreamVertex 1 Source [source]                              "String" "String"
+v2 = StreamVertex 2 Map    [[| filter (('#'==).head) . words |]] "String" "[String]"
 
-
-v1 = StreamVertex 1 Source [source]                                "String" "String"
-v2 = StreamVertex 2 Map    ["(filter (('#'==).head) . words)","s"] "String" "[String]"
-
-v5 = StreamVertex 5 Expand ["s"]                                   "[String]" "String"
-v6 = StreamVertex 6 Sink   ["mapM_ print"]                         "String" "String"
+v5 = StreamVertex 5 Expand []                                    "[String]" "String"
+v6 = StreamVertex 6 Sink   [[| mapM_ print |]]                   "String" "String"
 
 graph = path [v1, v2, v5, v6]
 
 parts = [[1,2],[5,6]]
 
-main = partitionGraph graph parts opts
+main = partitionGraph graph parts defaultOpts
