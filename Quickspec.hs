85115e972dd8c097c36160b5d86b1d67c78991e0
Jonathan Dowland <jon@dow.land>
2022-04-08 14:39:09 +0100

    Quickspec: add both, in-built funs

--- Quickspec.hs
+++ Quickspec.hs
@@ -12,9 +12,41 @@ main = quickSpec
   , con "streamMerge"     (streamMerge     :: [Stream A] -> Stream A)
   , con "streamExpand"    (streamExpand    :: Stream [A] -> Stream A)
   , con "streamJoin"      (streamJoin      :: Stream A -> Stream B -> Stream ( A,B))
-  , con "streamWindow"    (streamWindow    :: (Stream A -> [Stream A]) -> Stream A -> Stream [A])
-  , con "streamFilterAcc" (streamFilterAcc :: (B -> A -> B) -> B -> (A -> B -> Bool) -> Stream A -> Stream A)
+
+  -- this rule significantly slows down QuickSpec
+  --
+  {-
+   -WARNING: The following types have no 'Arbitrary' instance declared.
+    You will not get any variables of the following types:
+  [Event Int] -> Int
+  [Event Int] -> [Int]
+  [Event Int] -> [[Int]]
+  [Event Int] -> [[Event Int]]
+   - -}
+--, con "streamWindow"    (streamWindow    :: (Stream A -> [Stream A]) -> Stream A -> Stream [A])
+
+-- including this causes either Stack Overflow or Out Of Memory on
+-- my systems
+--, con "streamFilterAcc" (streamFilterAcc :: (B -> A -> B) -> B -> (A -> B -> Bool) -> Stream A -> Stream A)
+
+  {-
+   -You will not get any variables of the following types:
+  [Event Int] -> Int
+  [Event Int] -> Bool
+  [Event [Int]] -> Int
+  [Event [Int]] -> Bool
+  [[Event Int]] -> Int
+  [[Event Int]] -> Bool
+  [Event (Int, Int)] -> Int
+  [Event (Int, Int)] -> Bool
+   - -}
 
   , inst (Sub Dict :: Ord A :- Ord (Event A))
   , inst (Sub Dict :: Arbitrary A :- Arbitrary (Event A))
+
+  , funs
+  , background [ con "both"  (both :: (A -> Bool) -> (A -> Bool) -> A -> Bool) ]
   ]
+
+
+both p q e = p e && q e
