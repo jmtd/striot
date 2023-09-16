8ca0e69150062e239e9573a8740e41d5b4401d92
Jonathan Dowland <jon@dow.land>
2023-09-16 22:35:06 +0100

    gadt: full operator pantheon, sans typed TH
    ServiceTime omitted. probably best as the first argument to all of them

--- gadt.hs
+++ gadt.hs
@@ -11,23 +11,18 @@ data Event a = Event { time    :: Maybe Timestamp
 type Stream a = [Event a]
 type Timestamp       = UTCTime
 
+type Selectivity = Double
+type ArrivalRate = Double
+
 data StreamProgram o where
-    StreamSource :: Quote m => m Exp                         -> StreamProgram o
-    StreamMap    :: Quote m => m Exp -> StreamProgram (m i)  -> StreamProgram o
-    StreamFilter :: Quote m => m Exp -> StreamProgram (m i)  -> StreamProgram i
-    StreamSink   :: Quote m => m Exp -> StreamProgram (m i)  -> StreamProgram o
+    StreamSource    :: Quote m => ArrivalRate -> m Exp ->                                           StreamProgram (Stream o)
+    StreamMap       :: Quote m => m Exp -> StreamProgram (Stream i) ->                              StreamProgram (Stream o)
+    StreamScan      :: Quote m => m Exp -> o -> StreamProgram (Stream i) ->                         StreamProgram (Stream o)
+    StreamFilter    :: Quote m => Selectivity -> m Exp -> StreamProgram (Stream i) ->               StreamProgram (Stream i)
+    StreamFilterAcc :: Quote m => Selectivity -> m Exp -> o -> m Exp -> StreamProgram (Stream o) -> StreamProgram (Stream o)
+    StreamMerge     ::            [StreamProgram (Stream i)] ->                                     StreamProgram (Stream i)
+    StreamJoin      ::            StreamProgram (Stream a) -> StreamProgram (Stream b) ->           StreamProgram (Stream (a,b))
+    StreamWindow    :: Quote m => m Exp -> StreamProgram (Stream a) ->                              StreamProgram (Stream [a])
+    StreamExpand    ::            StreamProgram (Stream [o]) ->                                     StreamProgram (Stream o)
+    StreamSink      :: Quote m => m Exp -> StreamProgram (Stream i) ->                              StreamProgram (Stream o)
 
--- we're not yet wrapping the types in 'Stream'
-
-
-{-
-λ> :t StreamSource [|| 3::Int ||] & StreamSink [|| mapM_ print ||]
-StreamSource [|| 3::Int ||] & StreamSink [|| mapM_ print ||]
-  :: (Quote m, Foldable t, Show a) =>
-     StreamProgram Int (m (t a -> IO ()))
- -}
-
-{-
-(fmap unType . examineCode) [|| 3::Int ||]
-SigE (LitE (IntegerL 3)) (ConT GHC.Types.Int)
- -}
