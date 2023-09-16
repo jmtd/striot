5abdd100b8bb56b8338b02cdac3d3cd86dd67e3c
Jonathan Dowland <jon@dow.land>
2023-09-16 20:29:05 +0100

    gadt.hs: eliminate input type variable, write StreamFilter

--- gadt.hs
+++ gadt.hs
@@ -4,13 +4,11 @@
 import Language.Haskell.TH
 import Data.Function ((&))
 
-data StreamProgram i o where
-    StreamSource :: Quote m => Code m o                                     -> StreamProgram ()    (m o)
-    StreamMap    :: Quote m => Code m (i -> o) -> StreamProgram x (m i)     -> StreamProgram i     (m o)
-    StreamFilter ::            (i -> Bool)                                  -> StreamProgram i     i
-    --  for StreamSink, the connecting StreamProgram needs to output its input type; but
-    --  we don't care what that program's *input* type is. 'x' is used as a "don't care".
-    StreamSink   :: Quote m => Code m o        -> StreamProgram x (m i)     -> StreamProgram i (m o)
+data StreamProgram o where
+    StreamSource :: Quote m => Code m o                                   -> StreamProgram (m o)
+    StreamMap    :: Quote m => Code m (i -> o)    -> StreamProgram (m i)  -> StreamProgram (m o)
+    StreamFilter :: Quote m => Code m (i -> Bool) -> StreamProgram (m i)  -> StreamProgram (m i)
+    StreamSink   :: Quote m => Code m (i -> o)    -> StreamProgram (m i)  -> StreamProgram (m o)
 
 -- we're not yet wrapping the types in 'Stream'
 
