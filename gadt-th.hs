7a5d17c07a90d1b45015bf18a2fdda5df2858bfe
Jonathan Dowland <jon@dow.land>
2023-09-19 21:48:21 +0100

    gadt-th: finish constructors, test data

--- gadt-th.hs
+++ gadt-th.hs
@@ -3,26 +3,38 @@
 
 import Language.Haskell.TH
 import Data.Function ((&))
-import           Data.Time    (UTCTime)
+import Striot.FunctionalProcessing
+import Striot.FunctionalIoTtypes
 
-data Event a = Event { time    :: Maybe Timestamp
-                         , value   :: Maybe a}
-     deriving (Eq, Ord, Show, Read)
-type Stream a = [Event a]
-type Timestamp       = UTCTime
-
+type StrExp a = Code Q a
+type StreamProg a = StreamProgram (Stream a)
 
 data StreamProgram o where
-    StreamSource :: Code Q o                                       -> StreamProgram (Stream o)
-    StreamMap    :: Code Q (i -> o)    -> StreamProgram (Stream i) -> StreamProgram (Stream o)
-    StreamFilter :: Code Q (i -> Bool) -> StreamProgram (Stream i) -> StreamProgram (Stream i)
-    StreamSink   :: Code Q (i -> o)    -> StreamProgram (Stream i) -> StreamProgram (o)
-    StreamExpand :: StreamProgram (Stream [o])                     -> StreamProgram (Stream o)
+  StreamSource    :: StrExp o -> StreamProg o
+  StreamSink      :: StrExp (i -> o) -> StreamProg i -> StreamProgram o
+  StreamMap       :: StrExp (i -> o) -> StreamProg i -> StreamProg o
+  StreamScan      :: StrExp (o -> a -> o) -> StrExp o -> StreamProg a -> StreamProg o
+  StreamFilter    :: StrExp (i -> Bool) -> StreamProg i -> StreamProg i
+  StreamFilterAcc :: StrExp (b -> i -> b) -> StrExp b -> StrExp (i -> b -> Bool) -> StreamProg i -> StreamProg i
+  StreamWindow    :: StrExp (Stream o -> [Stream o]) -> StreamProg o -> StreamProg [o]
+  StreamExpand    :: StreamProg [o] -> StreamProg o
+  StreamMerge     :: [StreamProg o] -> StreamProg o
+  StreamJoin      :: StreamProg a -> StreamProg b -> StreamProg (a,b)
 
-{-
-(fmap unType . examineCode) [|| 3::Int ||]
-SigE (LitE (IntegerL 3)) (ConT GHC.Types.Int)
- -}
+test1 :: StreamProgram (IO ())
+test1 = StreamSource [|| 3 ||]
+      & StreamFilter [|| (>5) ||]
+      & StreamFilter [|| (<8) ||]
+      & StreamWindow [|| chop 1 ||]
+      & StreamSink   [|| print ||]
+
+test2 :: StreamProg Integer -> StreamProgram (IO ())
+test2 = StreamSink   [|| print ||]
+      . StreamWindow [|| chop 1 ||]
+      . StreamFilter [|| (<8) ||]
+      . StreamFilter [|| (>5) ||]
+
+
 
 printProgram :: StreamProgram a -> IO ()
 printProgram = mapM_ print . reverse . doSomething
