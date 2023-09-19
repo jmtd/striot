42b500821a7b1004ac20209afc40fe5058d60c8a
Jonathan Dowland <jon@dow.land>
2023-09-19 20:33:45 +0100

    attempt to graft gadt-simplest into StreamGraph
    overlapping types for Eq StreamGraph!

--- src/Striot/StreamGraph.hs
+++ src/Striot/StreamGraph.hs
@@ -97,6 +97,65 @@ unQualifyNames = everywhere (\a -> mkT f a)
 -- |A graph representation of a stream-processing program.
 type StreamGraph = Graph StreamVertex
 
+instance Eq StreamGraph where
+    a == b = fromStreamGraph a == fromStreamGraph b
+
+------------------------------------------------------------------------------
+-- StreamProg type
+-- an alternative StreamGraph representation to address an issue with Eq
+
+data StreamProg = StreamProg Int StreamOperator [Exp] String String Double [StreamProg]
+    deriving (Show)
+
+-- deliberately ignore the vertexId element. If a rewrite rule causes vertex
+-- renumbering, but the structure is identical and the expressions the same,
+-- we consider them equal.
+instance Eq StreamProg where
+    (StreamProg id1 op1 exps1 int1 out1 st1 parents1) ==
+        (StreamProg id2 op2 exps2 int2 out2 st2 parents2) = and
+            [ op1      == op2
+            , exps1    == exps2
+            , int1     == int2
+            , out1     == out2
+            , st1      == st2
+            , parents1 == parents2
+            ]
+
+-- construct a partially-applied StreamProg from a StreamVertex; lacking the
+-- final parent StreamProg parameter.
+fromStreamVertex :: StreamVertex -> ([StreamProg] -> StreamProg)
+fromStreamVertex (StreamVertex v o p i ot s) = StreamProg v o (map deQ p) i ot s
+
+toStreamVertex :: StreamProg -> StreamVertex
+toStreamVertex (StreamProg v o p i ot st _) = StreamVertex v o (map return p) i ot st
+
+prop_tofromStreamVertex_idempotent sv = sv == toStreamVertex (fromStreamVertex sv [])
+
+fromStreamGraph :: StreamGraph -> StreamProg
+fromStreamGraph sg = let
+    sink = (snd . head . filter ((==) Sink . operator . snd) . edgeList) sg
+    in fromStreamGraph' sg sink
+
+fromStreamGraph' :: StreamGraph -> StreamVertex -> StreamProg
+fromStreamGraph' sg v = let
+    incoming = (map fst . filter ((==) v . snd) . edgeList) sg
+    in fromStreamVertex v $ map (fromStreamGraph' sg) incoming
+
+toStreamGraph :: StreamProg -> StreamGraph
+toStreamGraph sp@(StreamProg _ _ _ _ _ _ par) = let
+    v = toStreamVertex sp
+    in edges (map (\p -> (toStreamVertex p, v)) par)
+        `overlay` overlays (map toStreamGraph par)
+
+-- guard for QuickCheck properties
+haveEdgesToSink :: StreamGraph -> Bool
+haveEdgesToSink = not . null . filter ((==) Sink . operator . snd) . edgeList
+
+prop_tofromStreamGraph_idempotent sg = 
+    haveEdgesToSink sg ==> sg == toStreamGraph (fromStreamGraph sg)
+
+------------------------------------------------------------------------------
+
 -- |A collection of partitioned StreamGraphs
 type PartitionedGraph = ([StreamGraph], StreamGraph)
 
