d7b12f8f28bdd6e949daa414fe8d19bdf12e3244
Jonathan Dowland <jon@dow.land>
2023-11-23 17:01:58 +0000

    gadt-th: type unification problems with rewrite rules
    Could we use SYP/Generics?

--- gadt-th.hs
+++ gadt-th.hs
@@ -61,10 +61,61 @@ expCmp :: StrExp (b -> c) -> StrExp (a -> b) -> StrExp (a -> c)
 expCmp e1 e2 = [|| $$(e1) . $$(e2) ||]
 
 -- StreamMap f . streamMap g = streamMap (f . g)
-mapFuse :: StreamProgram o -> StreamProgram o
-mapFuse (StreamMap f (StreamMap g instream)) =
+mapFuse :: StreamProgram o -> Maybe (StreamProgram o)
+mapFuse (StreamMap f (StreamMap g instream)) = Just $
     StreamMap (expCmp f g) instream
+mapFuse _ = Nothing
 
-mapFuse' :: StreamProgram o -> StreamProgram o
-mapFuse' (StreamMap f (StreamMap g instream)) =
+mapFuse' :: StreamProgram o -> Maybe (StreamProgram o)
+mapFuse' (StreamMap f (StreamMap g instream)) = Just $
     StreamMap [|| $$(f) . $$(g) ||] instream
+mapFuse' _ = Nothing
+
+-- applying rewrite rules
+-- we would need to visit every node in a program to find if a rule
+-- could be applied, and if so, potentially change the structure of
+-- the whole program. Functor is not suitable due to the latter.
+-- Foldable perhaps?
+--
+{-
+instance Foldable (StreamProgram a) where
+    foldl :: (b -> a -> b) -> b -> StreamProgram a -> b
+    foldl f acc (StreamSource e) = f acc $$(e)
+    -- ‘e’ is used in a top-level splice, quasi-quote, or annotation,
+    -- and must be imported, not defined locally
+ -}
+
+{-
+-- without something like foldl (above), we would need to pattern-match
+-- every constructor. Note we're fucked on type here; we can't put the
+-- two parents of streamJoin in the same list because they're different
+-- types;
+graphParents :: Streamprogram a -> [StreamProgram b]
+graphParents (StreamSource _) = Nothing
+
+  (StreamSink _ p) = p
+  (StreamMap _ p) = p
+  (StreamScan _ _ p) = p
+  (StreamFilter _ p) = p
+  (StreamFilterAcc _ _ _ p) = p
+  (StreamWindow _ p) = p
+  (StreamExpand   )
+  (StreamMerge    )
+  (StreamJoin     )
+-}
+
+{-
+applyRule :: (StreamProgram a -> Maybe (StreamProgram b)) -> StreamProgram c -> StreamProgram c -- XXX: it might change the type?!
+applyRule r p = case r p of
+    Nothing -> p
+    Just p' -> p'
+-- oh god some complex type stuff to understand here. the above fails because a,b,c
+-- are universally quantified. IOW the compiler cannot reconcile StreamProgram b and
+-- StreamProgram c, so 'r p' is not acceptable
+ - -}
+
+-- what about Eq?
+instance Eq (StreamProgram o) where
+    (StreamSink exp1 par1) == (StreamSink exp2 par2) =
+        exp1 == exp2 && par1 == par2
+        -- GHC can't unify the types in StrExp here
